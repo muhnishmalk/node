4ec63a76da4e8cb75192919936e48307a36ba863
pablo <147505260+muhnishmalk@users.noreply.github.com>
2024-02-29 11:38:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! git-hub my new fin');
+  res.send('Hello, World! git-hub my new fin nkj');
 });
 
 app.listen(port, () => {
